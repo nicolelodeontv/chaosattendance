f1beaa9543e3d889a76326dd2c760f47871994a5
Nicole John Dela Cruz <njuandelacruz@gmail.com>
2026-09-19 16:14:46 +0800

    Fix admin edit action markup

--- src/components/admin-dashboard.tsx
+++ src/components/admin-dashboard.tsx
@@ -223,7 +223,6 @@ function SubmissionsTab() {
                   <svg width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
                     <path d="M3 6h18M8 6V4a1 1 0 0 1 1 1h6a1 1 0 0 1 1 1v2m3 0-1 14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2L4 6" />
                   </svg>
-                </button>
                   </button>
                 </div>
               </td>
