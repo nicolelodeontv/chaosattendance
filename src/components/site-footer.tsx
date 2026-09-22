055c3c6b61cb85567be1df57cefa046872ed08be
Nicole John Dela Cruz <njuandelacruz@gmail.com>
2026-09-22 10:38:28 +0800

    Make footer creator spacing explicit (#32)

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -1,7 +1,8 @@
 export function SiteFooter() {
   return (
     <footer className="site-footer">
-      <span>Chaos Attendance Tracker · Created by{" "}</span>
+      <span>Chaos Attendance Tracker · Created by</span>
+      <span aria-hidden="true">{"\u00A0"}</span>
       <a
         href="https://discord.com/users/396080330702061588"
         target="_blank"
