1073f20d926d3bcbc8987643d6a88731061d6f21
Nicole John Dela Cruz <njuandelacruz@gmail.com>
2026-09-22 10:14:11 +0800

    Add global site footer

---
+++ src/components/site-footer.tsx
@@ -0,0 +1,14 @@
+export function SiteFooter() {
+  return (
+    <footer className="site-footer">
+      <span>Chaos Attendance Tracker · Created by </span>
+      <a
+        href="https://discord.com/users/396080330702061588"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Michol
+      </a>
+    </footer>
+  );
+}
