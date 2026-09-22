d0f2649f491c25f63cca002e6e9812f88994232b
Nicole John Dela Cruz <njuandelacruz@gmail.com>
2026-09-22 10:32:19 +0800

    Fix footer spacing before creator link

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -1,7 +1,7 @@
 export function SiteFooter() {
   return (
     <footer className="site-footer">
-      <span>Chaos Attendance Tracker · Created by </span>
+      <span>Chaos Attendance Tracker · Created by{" "}</span>
       <a
         href="https://discord.com/users/396080330702061588"
         target="_blank"
